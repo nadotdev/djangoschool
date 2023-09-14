043ae22eeef553363dd7caf71ce64636ec546673
Partrick23 <phatthepro@gmail.com>
2023-09-14 12:47:05 +0700

    Generate Classroom Models * not working

--- classroom/models.py
+++ classroom/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from student.models import Student
 
 default_no_image = "https://firebasestorage.googleapis.com/v0/b/fir-64ff1.appspot.com/o/user.png?alt=media&token=2d2e18cb-8019-4e0d-b215-1f4ed4b85ba9"
 
@@ -20,5 +21,14 @@ class Teacher(models.Model):
     dob = models.DateField()
     phone = models.CharField(max_length=25, blank=False, null=False, unique=True)
     email = models.EmailField(unique=True, default="teacher-email@industry.com")
-    photo = models.ImageField(upload_to='uploads/', max_length=500)
+    photo = models.ImageField(upload_to='uploads/', null=True)
     created_at = models.DateField(auto_now=True)
+
+class ClassroomModel(models.Model):
+    room_number = models.CharField(max_length=5, null=False)
+    year = models.DateField()
+    grade_id = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now=True)
+    
